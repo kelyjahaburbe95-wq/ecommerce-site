5f746178b8491e0062e77115517152140ba38e52
kelyjahaburbe95-wq <kelyjahaburbe95@gmail.com>
2025-12-19 22:31:40 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,23 @@
-const API_URL = "https://TON_BACKEND_RENDER.onrender.com/products";
-
-let cart = [];
+const API_URL = "https://ecommerce-site-nij4.onrender.com/products";
 
 fetch(API_URL)
   .then(res => res.json())
   .then(products => {
     const container = document.getElementById("products");
 
-    products.forEach(product => {
+    products.forEach(p => {
       const div = document.createElement("div");
       div.className = "product";
       div.innerHTML = `
-        <h3>${product.name}</h3>
-        <p>Prix : ${product.price} €</p>
+        <h3>${p.name}</h3>
+        <p>Prix : ${p.price} €</p>
+        <button>Acheter</button>
+      `;
+      container.appendChild(div);
+    });
+  })
+  .catch(err => {
+    document.getElementById("products").innerText =
+      "Erreur chargement produits";
+    console.error(err);
+  });
